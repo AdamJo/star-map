dfef972cdace40c45f8851750199aef6597375de
AdamJo <adamj0101@gmail.com>
2017-04-10 22:51:28 -0700

    feature(Test) add jest test suite

---
+++ __test__/app.spec.js
@@ -0,0 +1,14 @@
+import { h, Component } from 'preact';
+import render from 'preact-render-to-string';
+import App from '../src/views/';
+
+describe('App', () => {
+  it('exports `App`', () => {
+    expect(typeof App).toBe('object');
+  });
+});
+
+test('snapshot is working!', () => {
+  const app = render(App);
+  expect(app).toMatchSnapshot();
+});
